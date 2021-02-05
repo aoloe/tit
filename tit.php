d80ad066b22c7a8344acda37629268e2dd05619d
ale rimoldi <ale@graphicslab.org>
2021-02-05 20:54:38 +0100

    add a template engine

--- tit.php
+++ tit.php
@@ -44,6 +44,34 @@ if (get_magic_quotes_gpc()){
     foreach($_POST as $k=>$v) $_POST[$k] = stripslashes($v);
 }
 
+class TinyTemplate {
+    var $vars = [];
+    var $d = ['\{\{', '}}'];
+
+    static public function factory() {
+        return new TinyTemplate();
+    }
+
+    public function set_delimiter($start, $end) {
+        $this->d = [$start, $end];
+        return $this;
+    }
+
+    public function add($name, $value) {
+        $this->vars[$name] = $value;
+        return $this;
+    }
+
+    public function fetch($html) {
+        return preg_replace_callback("/{$this->d[0]}([a-z_-]+){$this->d[1]}/",
+                function($m) {
+                  return array_key_exists($m[1], $this->vars) ? $this->vars[$m[1]] : '';
+                },
+                $html);
+    }
+}
+
+
 // Here we go...
 session_start();
 
